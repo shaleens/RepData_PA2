fa766301e0f36f66635a88e748958b73ddad1668
Shaleen Kumar Sharma <sks@Shaleens-MacBook-Pro.local>
2014-09-19 01:10:05 +0530

    Incorporating exponents

--- testCode.R
+++ testCode.R
@@ -1,13 +1,12 @@
 data <- read.csv("repdata-data-StormData.csv")
 
-validMultiplierLetter <- c('B','K','M','X')
-multiplierValues <- c(1000000000, 1000, 1000000, 0)
+validMultiplierLetter <- c('B','K','M','1','2','3','4','5','6','7','8','XXX')
+multiplierValues <- c(9,3,6,1,2,3,4,5,6,7,8,0)
 names(multiplierValues) <- validMultiplierLetter
 
-propdmg.validMultipliers <- as.vector(sapply(toupper(data$PROPDMGEXP) , match, validMultiplierLetter, nomatch=4))
-data$PROPDMGEXP <- as.vector(multiplierValues[propdmg.validMultipliers])
+propdmg.validExponents <- as.vector(sapply(toupper(data$PROPDMGEXP) , match, validMultiplierLetter, nomatch=match('XXX',validMultiplierLetter)))
+data$PROPDMGEXP <- 10^as.numeric(multiplierValues[propdmg.validExponents])
 
-cropdmg.validMultipliers <- as.vector(sapply(toupper(data$CROPDMGEXP) , match, validMultiplierLetter, nomatch=4)) 
-data$CROPDMGEXP <- as.vector(multiplierValues[cropdmg.validMultipliers])
+cropdmg.validExponents <- as.vector(sapply(toupper(data$CROPDMGEXP) , match, validMultiplierLetter, nomatch=match('XXX',validMultiplierLetter))) 
+data$CROPDMGEXP <- 10^as.numeric(multiplierValues[cropdmg.validExponents])
 
-head(data[data$PROPDMG != 0, ])
