7cdfac882c1de4b13e8a18efa84a93a1a07d5a69
Shaleen Kumar Sharma <sks@Shaleens-MacBook-Pro.local>
2014-09-20 11:37:58 +0530

    Adding casualty related graphs

--- testCode.R
+++ testCode.R
@@ -22,3 +22,38 @@ propertydamage.top15Candidates <- propertydamage.byCause[order(propertydamage.by
 
 library(ggplot2)
 ggplot(data=propertydamage.top15Candidates, aes(x=cause, y=damage)) + geom_bar(aes(fill=cause),stat="identity") + scale_x_discrete(breaks=NULL) + labs(title="Top 15 hazards which damaged property the most")
+
+
+
+##Casualties
+
+casualties.deaths <- with(data, aggregate(FATALITIES, by = list(factor(EVTYPE)), sum))
+names(casualties.deaths) <- c('cause','deaths')
+casualties.deaths.top10 <- casualties.deaths[order(casualties.deaths$deaths, decreasing =T)[1:10],]
+
+casualties.injuries <- with(data, aggregate(INJURIES, by = list(factor(EVTYPE)), sum))
+names(casualties.injuries) <- c('cause','injuries')
+casualties.injuries.top10 <- casualties.injuries[order(casualties.injuries$injuries, decreasing = T)[1:10],]
+
+p1 <- ggplot(data=casualties.deaths.top10, aes(x=cause, y=deaths)) + geom_bar(aes(fill=cause),stat="identity") + scale_x_discrete(breaks=NULL) + labs(title="10 events with the most deaths") + theme(legend.text=element_text(size=7))
+p2 <- ggplot(data=casualties.injuries.top10, aes(x=cause, y=injuries)) + geom_bar(aes(fill=cause),stat="identity") + scale_x_discrete(breaks=NULL) + labs(title="10 events with the most injuries") + theme(legend.text=element_text(size=7))
+
+
+
+
+# ##Casualties
+# 
+# casualties.deaths <- with(data, aggregate(FATALITIES, by = list(factor(EVTYPE)), sum))
+# names(casualties.deaths) <- c('cause','deaths')
+# casualties.deaths.top5 <- casualties.deaths[order(casualties.deaths$deaths, decreasing =T)[1:5],]
+# 
+# casualties.injuries <- with(data, aggregate(INJURIES, by = list(factor(EVTYPE)), sum))
+# names(casualties.injuries) <- c('cause','injuries')
+# casualties.injuries.top5 <- casualties.injuries[order(casualties.injuries$injuries, decreasing = T)[1:5],]
+# 
+# library(grid)
+# library(gridExtra)
+# p1 <- ggplot(data=casualties.deaths.top5, aes(x=cause, y=deaths)) + geom_bar(aes(fill=cause),stat="identity") + scale_x_discrete(breaks=NULL) + labs(title="15 events with the most deaths")
+# p2 <- ggplot(data=casualties.injuries.top5, aes(x=cause, y=injuries)) + geom_bar(aes(fill=cause),stat="identity") + scale_x_discrete(breaks=NULL) + labs(title="15 events with the most deaths")
+# 
+# grid.arrange(p1, p2, ncol=1)
